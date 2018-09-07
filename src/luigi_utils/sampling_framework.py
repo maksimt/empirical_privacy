aa1d61edeaa3ed21254eca909ec41684c014e9b0
maksimt <maksimtsikhanovich@gmail.com>
2018-09-07 00:13:49 -0400

    re-use validation set across trials

--- src/luigi_utils/sampling_framework.py
+++ src/luigi_utils/sampling_framework.py
@@ -119,13 +119,13 @@ class _EvaluateStatisticalDistance(
         reqs['samples_positive'] = self.samplegen(
             dataset_settings=self.dataset_settings,
             num_samples=int(round(self.validation_set_size / 2)),
-            random_seed=self.random_seed + 'validation',
+            random_seed='validation',
             generate_positive_samples=True
             )
         reqs['samples_negative'] = self.samplegen(
             dataset_settings=self.dataset_settings,
             num_samples=int(round(self.validation_set_size / 2)),
-            random_seed=self.random_seed + 'validation',
+            random_seed='validation',
             generate_positive_samples=False
             )
         return reqs
