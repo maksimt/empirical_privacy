03a454153c8facd4570e1b0d49cb01bc4a152c29
maksimt <maksimtsikhanovich@gmail.com>
2018-09-09 15:47:41 -0400

    get ml1m user functionality added and tested

--- src/dataset_utils/recsys_datasets.py
+++ src/dataset_utils/recsys_datasets.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 import scipy as sp
 from sklearn.model_selection import train_test_split
+from joblib import Memory
+
+from empirical_privacy.config import LUIGI_COMPLETED_TARGETS_DIR
+
+
+memory = Memory(cachedir=LUIGI_COMPLETED_TARGETS_DIR, verbose=0)
 
 
 def load_dataset(dataset_name):
@@ -22,8 +28,68 @@ def load_dataset(dataset_name):
 
 
 def load_ml1m(test_size=0.3, random_state=0):
-    file_path = '/datasets/ml-1m/ratings.dat'
+    UI, R, n, d = get_ml1m_ratings()
 
+    UItr, UIte, Rtr, Rte = train_test_split(
+        UI,
+        R,
+        test_size=test_size,
+        random_state=random_state,
+        stratify=None
+        )
+
+    Xtr = sp.sparse.coo_matrix((Rtr, (UItr[:, 0], UItr[:, 1])),
+                               shape=(n, d)).tocsr()
+
+    Xte = sp.sparse.coo_matrix((Rte, (UIte[:, 0], UIte[:, 1])),
+                               shape=(n, d)).tocsr()
+
+    return {'Xtr': Xtr, 'Xte': Xte, 'ytr': None, 'yte': None}
+
+def get_ml1m_user(user_ind, test_size=0.3, random_state=0):
+    UI, R, n, d, I_ind = get_ml1m_ratings()
+
+    I_ind_inv = {v: k for (k, v) in I_ind.items()}
+
+    UItr, UIte, Rtr, Rte = train_test_split(
+        UI,
+        R,
+        test_size=test_size,
+        random_state=random_state,
+        stratify=None
+        )
+
+    U_user = UItr[:, 0] == user_ind
+    movies_rated = UItr[U_user, 1]
+    ratings = Rtr[U_user]
+
+    DF_movies = pd.DataFrame.from_csv(
+        '/datasets/ml-1m/movies.dat',
+        sep='::',
+        index_col=None,
+        header=None,
+        )
+    DF_movies.columns = ['MovieID', 'Title', 'Genres']
+
+    DF_users = pd.DataFrame.from_csv(
+        '/datasets/ml-1m/users.dat',
+        sep='::',
+        index_col=None,
+        header=None
+        )
+    DF_users.columns = ['UserID', 'Gender', 'Age', 'Occupation', 'Zip-code']
+
+    User_data = DF_users[DF_users.UserID == user_ind + 1]
+    Movies_rated = DF_movies[
+        DF_movies.MovieID.isin(I_ind_inv[j] for j in movies_rated)]
+    Movies_rated.loc[:, 'rating'] = \
+        [ratings[movies_rated == I_ind[i]][0] for i in Movies_rated.MovieID]
+
+    return {'User':User_data, 'Ratings':Movies_rated}
+
+@memory.cache
+def get_ml1m_ratings():
+    file_path = '/datasets/ml-1m/ratings.dat'
     DF = pd.DataFrame.from_csv(file_path, sep='::', index_col=None, header=None)
     DF.columns = pd.Index(['UserID', 'MovieID', 'Rating', 'Timestamp'],
                           dtype='int64')
@@ -38,9 +104,10 @@ def load_ml1m(test_size=0.3, random_state=0):
         U_ind[u] = pos
     for (pos, i) in enumerate(I_arg):
         I_ind[i] = pos
-    U_matrix = DF.loc[:, 'UserID'].as_matrix()
-    I_matrix = DF.loc[:, 'MovieID'].as_matrix()
-    R_matrix = DF.loc[:, 'Rating'].as_matrix()
+
+    U_matrix = DF.loc[:, 'UserID'].values
+    I_matrix = DF.loc[:, 'MovieID'].values
+    R_matrix = DF.loc[:, 'Rating'].values
     I_matrix = list(map(lambda i: I_ind[i], I_matrix))
     U_matrix = list(map(lambda u: U_ind[u], U_matrix))
 
@@ -49,20 +116,6 @@ def load_ml1m(test_size=0.3, random_state=0):
                     np.array(I_matrix[0:n]).reshape(n, 1)))
     R = np.array(R_matrix[0:n])
 
-    UItr, UIte, Rtr, Rte = train_test_split(
-        UI,
-        R,
-        test_size=test_size,
-        random_state=random_state,
-        stratify=None
-        )
-
     n, d = len(np.unique(U)), len(np.unique(I))
 
-    Xtr = sp.sparse.coo_matrix((Rtr, (UItr[:, 0], UItr[:, 1])),
-                               shape=(n, d)).tocsr()
-
-    Xte = sp.sparse.coo_matrix((Rte, (UIte[:, 0], UIte[:, 1])),
-                               shape=(n, d)).tocsr()
-
-    return {'Xtr': Xtr, 'Xte': Xte, 'ytr': None, 'yte': None}
+    return UI, R, n, d, I_ind
--- tests/unit/test_datasets.py
+++ tests/unit/test_datasets.py
@@ -1,10 +1,13 @@
 import pytest
+
 from dataset_utils import regression_datasets, text_datasets, recsys_datasets
+
 
 @pytest.mark.parametrize('name', ['MillionSongs', 'OnlineNewsPopularity'])
 def test_load_regression_dataset(name):
     ds = regression_datasets.load_dataset(name)
     assert ds['Xtr'].size > 1000
+
 
 @pytest.mark.parametrize('name', ['20NG'])
 def test_load_text_dataset(name):
@@ -12,8 +15,25 @@ def test_load_text_dataset(name):
     n, d = ds['Xtr'].shape
     assert n == 11314 and d > 5000
 
+
 @pytest.mark.parametrize('name', ['ml-1m'])
 def test_load_text_dataset(name):
     ds = recsys_datasets.load_dataset(name)
     n, d = ds['Xtr'].shape
-    assert n==6040 and d ==3706 and ds['Xtr'].shape == ds['Xte'].shape
+    assert n == 6040 and d == 3706 and ds['Xtr'].shape == ds['Xte'].shape
+
+
+def test_get_ml1m_user():
+    # 1::1193::5::978300760
+    # 1::661::3::978302109
+    # 1::914::3::978301968
+    # 1::3408::4::978300275
+    vals = recsys_datasets.get_ml1m_user(0)
+    assert vals['User'].Age[0] == 1
+    R = vals['Ratings']
+
+    assert R[R.MovieID == 1193].rating.iloc[0] == 5
+    assert R[R.MovieID == 661].rating.iloc[0] == 3
+    assert R[R.MovieID == 914].rating.iloc[0] == 3
+    assert R[R.MovieID == 3408].rating.iloc[0] == 4
+    print(vals)
